935b93a5e6528fdcc9c1b06c5e69fe56f830a7a6
Lars Ingebrigtsen <larsi@gnus.org>
2018-09-04 21:46:26 +0200

    Ensure that we have complete commands before parsing

--- z-stick.el
+++ z-stick.el
@@ -38,7 +38,8 @@ KERNEL==\"ttyACM[0-9]*\", SUBSYSTEM==\"tty\", SUBSYSTEMS==\"usb\", ATTRS{idProdu
   (with-current-buffer (get-buffer-create "*z-stick*")
     (erase-buffer)
     (set-buffer-multibyte nil)
-    (let ((proc (make-serial-process :port zs-device
+    (let ((start (point-min))
+	  (proc (make-serial-process :port zs-device
 				     :speed 115200
 				     :coding 'no-conversion
 				     :buffer (current-buffer))))
@@ -50,9 +51,49 @@ KERNEL==\"ttyACM[0-9]*\", SUBSYSTEM==\"tty\", SUBSYSTEMS==\"usb\", ATTRS{idProdu
 	   (with-current-buffer zs-buffer
 	     (goto-char (point-max))
 	     (insert string)
-	     (message "%s" string)
+	     ;; We receive data in very small chunks from the Z-Stick,
+	     ;; and we have to collect the output until we get a
+	     ;; complete command until we start to parse.
+	     (when (zs-complete-command-p start)
+	       (goto-char start)
+	       (let ((commands (zs-parse)))
+		 (message "%s" commands)
+		 (setq start (point))
+		 (dolist (command commands)
+		   (let ((callback (intern (format "zs-callback-%s"
+						   (getf command :command))
+					   obarray)))
+		     (when (fboundp callback)
+		       (funcall callback command))))))
 	     ;; ACK the message we got from the Z-Stick.
 	     (zs-send '(#x06) t))))))))
+
+(defun zs-callback-receive-status-type-broad (command)
+  (let* ((slots '((2 status)
+		  (3 node)
+		  (5 class-id)
+		  (4 unk1)
+		  (6 counter)
+		  (8 sub-node)))
+	 (message
+	  (loop for (index slot) in slots
+		append (list (intern (format ":%s" slot) obarray)
+			     (elt (getf command :data) (1- index))))))
+    (message "%s %s" message (getf command :data))))
+
+(defun zs-complete-command-p (point)
+  (save-excursion
+    (goto-char point)
+    (unless (eobp)
+      (let ((type (cadr (assq (zs-read) zs-transaction-types))))
+	(cond
+	 ((not (eq type 'sof))
+	  t)
+	 ((eobp)
+	  nil)
+	 (t
+	  (let ((length (zs-read)))
+	    (>= (- (point-max) (point)) length))))))))
 
 (defvar zs-transaction-types
   '((#x01 sof)				; Start of field
@@ -82,56 +123,78 @@ KERNEL==\"ttyACM[0-9]*\", SUBSYSTEM==\"tty\", SUBSYSTEMS==\"usb\", ATTRS{idProdu
     (#x23 read-memory)
 
     (#x40 set-learn-node-state)		; not implemented
-    (#x41 get-node-protocol-info) ; get protocol info (baud rate, listening, etc.) for a given node
-    (#x42 set-default) ; reset controller and node info to default (original) values
+    (#x41 get-node-protocol-info) ; get protocol info (baud rate,
+				  ; listening, etc.) for a given node
+    (#x42 set-default) ; reset controller and node info to default
+		       ; (original) values
     (#x43 new-controller)		; not implemented
-    (#x44 replication-command-complete) ; replication send data complete
+    (#x44 replication-command-complete) ; replication send data
+					; complete
     (#x45 replication-send-data)	; replication send data
-    (#x46 assign-return-route) ; assign a return route from the specified node to the controller
-    (#x47 delete-return-route) ; delete all return routes from the specified node
-    (#x48 request-node-neighbor-update) ; ask the specified node to update its neighbors (then read them from the controller)
-    (#x49 application-update) ; get a list of supported (and controller) command classes
-    (#x4a add-node-to-network) ; control the addnode (or addcontroller) process...start, stop, etc.
-    (#x4b remove-node-from-network) ; control the removenode (or removecontroller) process...start, stop, etc.
-    (#x4c create-new-primary) ; control the createnewprimary process...start, stop, etc.
-    (#x4d controller-change) ; control the transferprimary process...start, stop, etc.
-    (#x50 set-learn-mode) ; put a controller into learn mode for replication/ receipt of configuration info
+    (#x46 assign-return-route) ; assign a return route from the
+			       ; specified node toq the controller
+    (#x47 delete-return-route) ; delete all return routes from the
+			       ; specified node
+    (#x48 request-node-neighbor-update) ; ask the specified node to
+					; update its neighbors (then
+					; read them from the
+					; controller)
+    (#x49 application-update) ; get a list of supported (and
+			      ; controller) command classes
+    (#x4a add-node-to-network) ; control the addnode (or
+			       ; addcontroller) process...start, stop,
+			       ; etc.
+    (#x4b remove-node-from-network) ; control the removenode (or
+				    ; removecontroller)
+				    ; process...start, stop, etc.
+    (#x4c create-new-primary) ; control the createnewprimary
+			      ; process...start, stop, etc.
+    (#x4d controller-change) ; control the transferprimary
+			     ; process...start, stop, etc.
+    (#x50 set-learn-mode) ; put a controller into learn mode for
+			  ; replication/ receipt of configuration info
     (#x51 assign-suc-return-route)  ; assign a return route to the suc
     (#x52 enable-suc)	; make a controller a static update controller
     (#x53 request-network-update)	; network update for a suc(?)
     (#x54 set-suc-node-id) ; identify a static update controller node id
     (#x55 delete-suc-return-route)   ; remove return routes to the suc
-    (#x56 get-suc-node-id) ; try to retrieve a static update controller node id (zero if no suc present)
-    (#x5a request-node-neighbor-update-options) ; allow options for request node neighbor update
+    (#x56 get-suc-node-id) ; try to retrieve a static update
+			   ; controller node id (zero if no suc
+			   ; present)
+    (#x5a request-node-neighbor-update-options) ; allow options for
+						; request node
+						; neighbor update
     (#x5e explore-request-inclusion)		; supports nwi
-    (#x60 request-node-info) ; get info (supported command classes) for the specified node
+    (#x60 request-node-info) ; get info (supported command classes)
+			     ; for the specified node
     (#x61 remove-failed-node-id)  ; mark a specified node id as failed
-    (#x62 is-failed-node-id) ; check to see if a specified node has failed
-    (#x63 replace-failed-node) ; remove a failed node from the controller's list (?)
-    (#x80 get-routing-info) ; get a specified node's neighbor information from the controller
-    (#xa0 serial-api-slave-node-info) ; set application virtual slave node information
+    (#x62 is-failed-node-id) ; check to see if a specified node has
+			     ; failed
+    (#x63 replace-failed-node) ; remove a failed node from the
+			       ; controller's list (?)
+    (#x80 get-routing-info) ; get a specified node's neighbor
+			    ; information from the controller
+    (#xa0 serial-api-slave-node-info) ; set application virtual slave
+				      ; node information
     (#xa1 application-slave-command-handler) ; slave command handler
     (#xa2 send-slave-node-info)	 ; send a slave node information frame
     (#xa3 send-slave-data)	 ; send data from slave
     (#xa4 set-slave-learn-mode)	 ; enter slave learn mode
     (#xa5 get-virtual-nodes)	 ; return all virtual nodes
     (#xa6 is-virtual-node)	 ; virtual node test
-    (#xd0 set-promiscuous-mode) ; set controller into promiscuous mode to listen to all frames
+    (#xd0 set-promiscuous-mode) ; set controller into promiscuous mode
+				; to listen to all frames
     (#xd1 promiscuous-application-command-handler)))
 
-(defun zs-parse (string)
+(defun zs-parse ()
   (let ((elems nil))
-    (with-temp-buffer
-      (set-buffer-multibyte nil)
-      (insert string)
-      (goto-char (point-min))
-      (while (not (eobp))
-	(push
-	 (let ((type (cadr (assq (zs-read) zs-transaction-types))))
-	   (if (eq type 'sof)
-	       (zs-parse-field)
-	     type))
-	 elems)))
+    (while (zs-complete-command-p (point))
+      (push
+       (let ((type (cadr (assq (zs-read) zs-transaction-types))))
+	 (if (eq type 'sof)
+	     (zs-parse-field)
+	   type))
+       elems))
     (nreverse elems)))
 
 (defun zs-read ()
